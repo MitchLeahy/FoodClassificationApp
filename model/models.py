c7ed2408fa8fd17cb76ed53a881aefba2ec67d0d
Mitch Leahy <leahy.mi@northeastern.edu>
2023-07-20 20:13:01 -0400

    add tensor fix

--- model/models.py
+++ model/models.py
@@ -1,41 +1,41 @@
-# from _models import load_model, predict
-# from image_preprocessing import preprocess_image
+from _models import load_model, predict
+from image_preprocessing import preprocess_image
 import tensorflow as tf
 
-from PIL import Image
-import numpy as np
+# from PIL import Image
+# import numpy as np
 
-def preprocess_image(uploaded_file):
-    # Open the image file
-    img = Image.open(uploaded_file)
+# def preprocess_image(uploaded_file):
+#     # Open the image file
+#     img = Image.open(uploaded_file)
     
-    # Resize the image to 224x224 pixels (this size is commonly used by image processing CNNs)
-    img = img.resize((224, 224))
+#     # Resize the image to 224x224 pixels (this size is commonly used by image processing CNNs)
+#     img = img.resize((224, 224))
     
-    # Convert the image to a numpy array
-    img_array = np.array(img)
+#     # Convert the image to a numpy array
+#     img_array = np.array(img)
 
-    # If the image is grayscale, convert it to RGB
-    if len(img_array.shape) == 2:
-        img_array = np.repeat(img_array[:, :, np.newaxis], 3, axis=2)
+#     # If the image is grayscale, convert it to RGB
+#     if len(img_array.shape) == 2:
+#         img_array = np.repeat(img_array[:, :, np.newaxis], 3, axis=2)
     
-    # If the image has an alpha channel, remove it
-    if img_array.shape[2] == 4:
-        img_array = img_array[:, :, :3]
+#     # If the image has an alpha channel, remove it
+#     if img_array.shape[2] == 4:
+#         img_array = img_array[:, :, :3]
 
-    # Normalize the image array (this helps CNNs perform better)
-    img_array = img_array / 255.0
+#     # Normalize the image array (this helps CNNs perform better)
+#     img_array = img_array / 255.0
 
-    return img_array
-def load_model(model_path):
-    # Load the model from the h5 file
-    model = tf.keras.models.load_model(model_path)
-    return model
+#     return img_array
+# def load_model(model_path):
+#     # Load the model from the h5 file
+#     model = tf.keras.models.load_model(model_path)
+#     return model
 
-def predict(model, image):
-    # Use the model to make a prediction
-    prediction = model.predict(image)
-    return prediction
+# def predict(model, image):
+#     # Use the model to make a prediction
+#     prediction = model.predict(image)
+#     return prediction
 
 def is_food(image_path):
     # Load the model
