86e68e9e96ec6cf0b643a1fb25bf3e6228f15dda
Mitch Leahy <leahy.mi@northeastern.edu>
2023-07-20 20:38:16 -0400

    add tensor fix

--- model/_models.py
+++ model/_models.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 
 
 def load_model(model_path):
-    try:
-        # Load the model from the h5 file
-        model = tf.keras.models.load_model(model_path)
-        return model
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    # try:
+    # Load the model from the h5 file
+    model = tf.keras.models.load_model(model_path)
+    return model
+    # except Exception as e:
+    #     print(f"Error: {e}")
+    #     return None
 
 
 def predict(model, image):
